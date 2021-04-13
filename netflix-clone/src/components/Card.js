4be19253acf45afbcf1f1aab4934dacf8b838896
shakibmazhar <shakibmazhar77@gmail.com>
2021-04-13 20:23:04 +0600

    Dynamic card position

--- netflix-clone/src/components/Card.js
+++ netflix-clone/src/components/Card.js
@@ -3,12 +3,28 @@ import '../Card.css'
 
 function Card({title, poster, description, posX, posY}) {
     const base_URL = "https://image.tmdb.org/t/p/original/"
+
+    //Returns ... after specified character number
+    function turncate(str, n){
+        return str?.length > n ? str.substr(0, n-1) + " ..." : str
+    }
     
+    let style;
+    // console.log('X: ' + posX, 'Y: ' + posY)
       //Passing page relative cursor position to div style
-    const style = {
-        left: posX,
-        top: posY,
-        position: 'absolute'
+    if(posY < 2000){
+        style = {
+            left: posX,
+            top: posY,
+            position: 'absolute'
+        }
+    }
+    else{
+        style = {
+            left: posX,
+            top: posY -350,
+            position: 'absolute'
+        }
     }
     
     return (
@@ -22,7 +38,7 @@ function Card({title, poster, description, posX, posY}) {
             alt = 'Poster' />
             <div className="tool-body">
                 <h2>{title}</h2>
-                <p>{description}</p>
+                <p>{turncate(description, 400)}</p>
             </div>
         </div>
     )    
